eb6fadb426bc5b16fe3507cd810b97fd518117a8
Anypogers <gnutzmannenzo@gmail.com>
2024-09-10 10:53:03 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { Post } from "./Post";
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
 
 import styles from './App.module.css'
 import './global.css';
@@ -13,15 +13,9 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Machado de assis"
-            comment="Lorem ipsum dolor sit amet consectetur adipisicing elit."/>
-          <Post
-            author="Castro Alves"
-            comment="Lorem ipsum dolor sit amet consectetur adipisicing elit."/>
-          <Post
-            author="Padre Antonio Vieira"
-            comment="Lorem ipsum dolor sit amet consectetur adipisicing elit."/>
+          <Post />
+          <Post />
+          <Post />
         </main>
       </div>
     </div>
--- src/Post.jsx
+++
@@ -1,9 +0,0 @@
-export function Post(Props){
-  return(
-    <div>
-      <strong>{Props.author}</strong>
-        <p>{Props.comment}</p>
-    </div>
-  )
-}
-
